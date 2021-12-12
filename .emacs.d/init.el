0356d66ab711ea83f44f5c95873547b98d6789cb
rsk0315 <rsk0315@icloud.com>
2021-12-12 12:35:14 +0900

    [update] init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,3 +26,53 @@
 (add-hook 'ruby-mode-hook
           '(lambda ()
              (setq ruby-indent-level 4)))
+
+;; (require 'package)
+;; (add-to-list 'package-archives
+;;              '("melpa" . "https://melpa.org/packages/") t)
+;; (package-initialize)
+;; (package-refresh-contents)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(string-inflection ddskk haskell-mode yaml-mode markdown-mode rust-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'rust-mode)
+(setq rust-format-on-save t)
+
+;; (use-package markdown-mode
+;;              :ensure t
+;;              :commands (markdown-mode gfm-mode)
+;;              :mode (("README\\.md\\'" . gfm-mode)
+;;                     ("\\.md\\'" . markdown-mode)
+;;                     ("\\.markdown\\'" . markdown-mode))
+;;              :init (setq markdown-command "multimarkdown"))
+
+(setq load-path (append '("~/.emacs.d/elisp") load-path))
+
+(require 'satysfi)
+(require 'satysfi-mode)
+
+(add-to-list 'auto-mode-alist '("\\.\\(saty\\|satyh\\)$" . satysfi-mode))
+(autoload 'satysfi-mode "satysfi-mode" "Major mode for editing SATySFi document." t)
+
+;; (global-set-key (kbd "C-x C-j") 'skk-mode)
+;; (setq skk-tut-file "~/git/skk-dev/ddskk/etc/SKK.tut")
+
+;; https://stackoverflow.com/questions/9288181/converting-from-camelcase-to-in-emacs
+(global-set-key (kbd "C-c i") 'string-inflection-cycle)
+(global-set-key (kbd "C-c C") 'string-inflection-camelcase)          ;; UpperCamelCase
+(global-set-key (kbd "C-c c") 'string-inflection-lower-camelcase)    ;; lowerCamelCase
+(global-set-key (kbd "C-c -") 'string-inflection-kebab-case)         ;; kebab-case
+(global-set-key (kbd "C-c _") 'string-inflection-underscore)         ;; kebab-case
+(global-set-key (kbd "C-c =") 'string-inflection-capital-underscore) ;; kebab-case
